db448d5b68a8c139e486271a5a6748483226a5e4
Sergio Asensio Ruiz <serasensio@gmail.com>
2021-04-13 02:38:48 +0200

    final Sprint 1

--- public/javascripts/searchrecipes.js
+++ public/javascripts/searchrecipes.js
@@ -1,16 +1,32 @@
+/**
+ * @fileoverview Cerca de receptes personalitzat: 
+ * <p> Mostra per pantalla el resultat de la cerca personalitzada.</p>
+ * 
+ * <p> History</p>
+ * 0.1 - Implementació de la cerca de receptes.  
+ *  
+ * @version     0.1
+ * @author      Sergio Asensio Ruiz 
+ * @copyright   bienmesabe.com
+ * 
+ */
+
 $(document).ready(function () {
-    
 
-    let $id = (window.location.search).substr(8,)
-
-    console.log($id)
+    let $urlSearch = 'http://localhost:8080/api/recipe/getRecipesByFilters/';
+    let $search = (window.location.search).substr(1, );
 
     $.ajax({
-        url: 'http://localhost:8080/api/recipe/getRecipesById/'+$id,
+        url: $urlSearch + $search,
         type: 'GET',
-        success: function (data){
-            console.log(data)
-            $('.recetas').append('<span> Receta: '+data.name)
+        success: function (data) {
+            for (let i = 0; i < data.length; i++) {
+                $('.recetas').append('<div style="border: 1px solid #CED4DA;display: flex;align-items: center;border-radius: .25rem;max-width: 60%;margin-left: 10px;padding: 0px 10px;">\
+                                        <span id="recipes" style="min-width: 275px;"><text style="font-weight:700;">Receta:</text> ' + data[i].name + '</span>\
+                                        <img style="width: 50%;padding:10px;border-radius: 1.25rem;" src="' + data[i].image + '"></img>\
+                                        </div>\
+                                    </div>');
+            }
         }
-    })
+    });
 });
