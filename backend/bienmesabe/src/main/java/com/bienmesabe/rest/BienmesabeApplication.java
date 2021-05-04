f60b20f13c81c7cf94c4d1a4368011f428c8b30c
raul <starlord.rrc@gmail.com>
2021-05-04 20:13:18 +0200

    Deshabilitat cors

--- backend/bienmesabe/src/main/java/com/bienmesabe/rest/BienmesabeApplication.java
+++ backend/bienmesabe/src/main/java/com/bienmesabe/rest/BienmesabeApplication.java
@@ -41,7 +41,7 @@ public class BienmesabeApplication {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
+			http.cors().and().csrf().disable()
 				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
 				.authorizeRequests()
 				.antMatchers(HttpMethod.GET, "/api/recipe").permitAll()
